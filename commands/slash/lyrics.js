c9645731749a0c52350344350a3d67b958ed14a3
Andrew <vitrage@protonmail.com>
2022-06-06 22:47:45 +0300

    fix Double X in lyrics and loop command (sudhan)

--- commands/slash/lyrics.js
+++ commands/slash/lyrics.js
@@ -15,38 +15,38 @@ const command = new SlashCommand()
   )
   .setRun(async (client, interaction, options) => {
 	await interaction.deferReply().catch((_) => {});
-	
+
 	await interaction.editReply({
 		embeds: [client.Embed(":mag_right: **Searching...**")],
 	});
-	
+
 	const args = interaction.options.getString("song");
-	
+
 	let player = client.manager.players.get(interaction.guild.id);
-	
+
 	if (!args && !player)
 	return interaction.editReply({
 		embeds: [client.Embed("❌ | **There's nothing playing**")],
 	});
-	
+
 	// if no input, search for the current song. if no song console.log("No song input");
 	let search = args ? args : player.queue.current.title;
 	let url = `https://api.darrennathanael.com/lyrics?song=${search}`;
-	
-	
-	// get the lyrics 
+
+
+	// get the lyrics
 	let lyrics = await fetch(url).then((res) => res.json());
-	
-	// if the status is not ok then send error embed and return	
+
+	// if the status is not ok then send error embed and return
 	if (lyrics.response !== 200) {
 		let failEmbed = new MessageEmbed()
 		.setColor("RED")
 		.setDescription(`❌ | No lyrics found for ${search}! Please try again.`);
 		return interaction.editReply({ embeds: [failEmbed] });
 	}
-	
+
 	let text = lyrics.lyrics;
-	
+
 	// check for pagination, if needed construct appropriate components, initialize first string to displate on page [0]
 	if(text.length > 4090){
 		text = text.substring(0, 4090) + "...";
@@ -59,11 +59,11 @@ const command = new SlashCommand()
 
 		let maxPages = Math.ceil(lyrics.lyrics.length / 4090);
 
-		
+
 		// default page
 		let pageNo = 0;
-		
-		
+
+
 		// Construction of the buttons for the embed in case pagination is needed
 		const getButtons = (pageNo) => {
 			return new MessageActionRow().addComponents(
@@ -82,18 +82,18 @@ const command = new SlashCommand()
 
 		const tempMsg = await interaction.editReply({ embeds: [lyricsEmbed], components: [getButtons(pageNo)], fetchReply: true });
 		const collector = tempMsg.createMessageComponentCollector({ time: 600000, componentType: "BUTTON" });
-	
+
 		collector.on("collect", async (iter) => {
 			if (iter.customId === "lyrics_pag_but_1_app") {
 				pageNo++;
 			} else if (iter.customId === "lyrics_pag_but_2_app") {
 				pageNo--;
 			}
-			
+
 			text = lyrics.lyrics.substring(pageNo * 4090, (pageNo * 4090) + 4090);
-			
+
 			lyricsEmbed.setDescription(text);
-			
+
 			await iter.update({ embeds: [lyricsEmbed], components: [getButtons(pageNo)], fetchReply: true });
 		});
 	} else {
@@ -107,4 +107,4 @@ const command = new SlashCommand()
 	}
 });
 
-module.exports = command;	
+module.exports = command;

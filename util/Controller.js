6549ed0e82a925e55cb8783b6ea2c2753db7cc0f
Andrew <vitrage@protonmail.com>
2022-06-07 16:58:58 +0300

    Buttons change, emojis to labels, Controller.js ukrainization and color fixes

--- util/Controller.js
+++ util/Controller.js
@@ -12,7 +12,7 @@ module.exports = async (client, interaction) => {
   if (!player) {
     interaction.reply({
       embeds: [
-        client.Embed("❌ | **There is no player to control in this server.**"),
+        client.ErrorEmbed("Нічого не грає, нічого контролювати"),
       ],
     });
     setTimeout(() => {
@@ -22,9 +22,9 @@ module.exports = async (client, interaction) => {
   }
   if (!interaction.member.voice.channel) {
     const joinEmbed = new MessageEmbed()
-      .setColor(client.config.embedColor)
+      .setColor("RED")
       .setDescription(
-        "❌ | **You must be in a voice channel to use this command!**"
+        "❌ | Хазяїн, ви не в голосовому каналі"
       );
     return interaction.reply({ embeds: [joinEmbed], ephemeral: true });
   }
@@ -34,21 +34,35 @@ module.exports = async (client, interaction) => {
     !interaction.guild.me.voice.channel.equals(interaction.member.voice.channel)
   ) {
     const sameEmbed = new MessageEmbed()
-      .setColor(client.config.embedColor)
+      .setColor("RED")
       .setDescription(
-        "❌ | **You must be in the same voice channel as me to use this command!**"
+        "❌ | Хазяїн, ви не в моєму голосовому каналі"
       );
     return interaction.reply({ embeds: [sameEmbed], ephemeral: true });
   }
-  if (property === "LowVolume") {
-    player.setVolume(player.volume - 10);
+  // if (property === "LowVolume") {
+  //   player.setVolume(player.volume - 10);
+  //   interaction.reply({
+  //     embeds: [
+  //       client.Embed(
+  //         "🔉 | **Successfully lowered server volume to** `" +
+  //           player.volume +
+  //           "%`"
+  //       ),
+  //     ],
+  //   });
+  //   setTimeout(() => {
+  //     interaction.deleteReply();
+  //   }, 5000);
+  //   return;
+  // }
+
+  //TODO: зробити так, щоб після виконання цієї команди кнопки видалялись
+  if (property === "Stop") {
+    player.destroy();
     interaction.reply({
       embeds: [
-        client.Embed(
-          "🔉 | **Successfully lowered server volume to** `" +
-            player.volume +
-            "%`"
-        ),
+        client.Embed(`Від'єднався!`),
       ],
     });
     setTimeout(() => {
@@ -61,7 +75,7 @@ module.exports = async (client, interaction) => {
   if (property === "Replay") {
     if (!player.queue.previous) {
       interaction.reply({
-        embeds: [client.Embed("❌ | **There is no previous song to replay.**")],
+        embeds: [client.ErrorEmbed("Попередньої пісні для повторення немає")],
       });
       setTimeout(() => {
         interaction.deleteReply();
@@ -81,8 +95,8 @@ module.exports = async (client, interaction) => {
       embeds: [
         client.Embed(
           player.paused
-            ? ":white_check_mark: | **Paused**"
-            : ":white_check_mark: | **Resumed**"
+            ? ":white_check_mark: | На паузі"
+            : ":white_check_mark: | Знову грає"
         ),
       ],
     });
@@ -97,39 +111,42 @@ module.exports = async (client, interaction) => {
     return interaction.deferUpdate();
   }
 
-  if (property === "HighVolume") {
-    // increase volume by 10% else if volume at 200% do nothing
-    if (player.volume < 125) {
-      player.setVolume(player.volume + 5);
-      interaction.reply({
-        embeds: [
-          client.Embed(
-            "🔊 | **Successfully increased server volume to** `" +
-              player.volume +
-              "%`"
-          ),
-        ],
-      });
-      setTimeout(() => {
-        interaction.deleteReply();
-      }, 5000);
-    } else {
-      interaction.reply({
-        embeds: [
-          client.Embed(
-            "👍 | **Volume is at maximum** `" + player.volume + "%`"
-          ),
-        ],
-      });
-      setTimeout(() => {
-        interaction.deleteReply();
-      }, 5000);
-    }
-    return;
-  }
+  // if (property === "HighVolume") {
+  //   // increase volume by 10% else if volume at 200% do nothing
+  //   if (player.volume < 125) {
+  //     player.setVolume(player.volume + 5);
+  //     interaction.reply({
+  //       embeds: [
+  //         client.Embed(
+  //           "🔊 | **Successfully increased server volume to** `" +
+  //             player.volume +
+  //             "%`"
+  //         ),
+  //       ],
+  //     });
+  //     setTimeout(() => {
+  //       interaction.deleteReply();
+  //     }, 5000);
+  //   } else {
+  //     interaction.reply({
+  //       embeds: [
+  //         client.Embed(
+  //           "👍 | **Volume is at maximum** `" + player.volume + "%`"
+  //         ),
+  //       ],
+  //     });
+  //     setTimeout(() => {
+  //       interaction.deleteReply();
+  //     }, 5000);
+  //   }
+  //   return;
+  // }
 
   return interaction.reply({
-    ephemeral: true,
-    content: "❌ | **Unknown controller option**",
+    embeds: [
+      client.ErrorEmbed("Невідома опція контролера")
+    ],
+    ephemeral: true
+    //content: "❌ | Невідома опція контролера",
   });
 };

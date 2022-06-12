79850279225aa3a3f3f6de9be64edd70300e14b3
Darren <github@darrennathanael.com>
2022-06-12 04:08:16 -0500

    🐷🐝🔪🔪🦧 # Conflicts: #	index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const DiscordMusicBot = require("./lib/DiscordMusicBot");
 const { exec } = require("child_process");
 
 if (process.env.REPL_ID) {
-  console.log("Replit system detected, initiating special `unhandledRejection` event listener | index.js:19")
-  process.on('unhandledRejection', (reason, promise) => {
-    promise.catch((err) => {
-      if (err.status === 429) {
-        console.log("something went wrong while logging in, resetting...");
-        exec("kill 1");
-      }
-    });
-  });
+	console.log("Replit system detected, initiating special `unhandledRejection` event listener.")
+	process.on('unhandledRejection', (reason, promise) => {
+		promise.catch((err) => {
+			if (err.status === 429) {
+				console.log("something went wrong whilst trying to connect to discord gateway, resetting...");
+				exec("kill 1");
+			}
+		});
+	});
 }
 
 const client = new DiscordMusicBot();

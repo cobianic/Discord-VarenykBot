67e4f37dbc71f52e4c177ff28f7cfc1450e4ec41
Amiyo <84950599+DevAmiyo@users.noreply.github.com>
2022-07-04 12:25:27 +0530

    Set 24/7 and autoqueue in config  (#952)
    feat: set 24/7 and autoqueue in config
    # Conflicts:
    #	config.js

--- config.js
+++ config.js
@@ -1,18 +1,20 @@
 module.exports = {
-	cmdPerPage: 10, //Number of commands per page of help command
-	adminId: "288305644807782400", // Admin of the bot
-	token: process.env.token || "", //Bot's Token
-	clientId: process.env.clientId || "", //ID of the bot
-	clientSecret: process.env.clientSecret || "", //Client Secret of the bot
-	port: 4200, //Port of the API and Dashboard
-	scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
-	serverDeafen: true, //If you want bot to stay deafened
-	defaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
-	supportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-	Issues: "https://github.com/SudhanPlayz/Discord-MusicBot/issues", //Bug Report Link
-	permissions: 277083450689, //Bot Inviting Permissions
-	disconnectTime: 1, //How long should the bot wait before disconnecting from the voice channel. in miliseconds. set to 1 for instant disconnect.
-	alwaysplay: true, // when set to true music will always play no matter if theres no one in voice channel.
+	cmdPerPage: 10, //- Number of commands per page of help command
+	adminId: "288305644807782400", //- Replace UserId with the Discord ID of the admin of the bot
+	token: process.env.token || "", //- Bot's Token
+	clientId: process.env.clientId || "", //- ID of the bot
+	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
+	port: 4200, //- Port of the API and Dashboard
+	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
+	serverDeafen: true, //- If you want bot to stay deafened
+	defaultVolume: 100, //- Sets the default volume of the bot, You can change this number anywhere from 1 to 100
+	supportServer: "https://discord.gg/sbySMS7m3v", //- Support Server Link
+	Issues: "https://github.com/SudhanPlayz/Discord-MusicBot/issues", //- Bug Report Link
+	permissions: 277083450689, //- Bot Inviting Permissions
+	disconnectTime: 1, //- How long should the bot wait before disconnecting from the voice channel. in miliseconds. set to 1 for instant disconnect.
+	twentyFourSeven: false, //- When set to true, the bot will never disconnect from the voice channel
+	autoQueue: false, //- When set to true, related songs will automatically be added to the queue
+	alwaysplay: true, //- When set to true music will always play no matter if theres no one in voice channel.
 	debug: false, //Debug mode
 	cookieSecret: "CodingWithSudhan is epic",//Cookie
 	website: "http://localhost:4200",//without the / at the end
@@ -21,9 +23,9 @@ module.exports = {
 	nodes: [
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "lavalink.oops.wtf",
-			port: 2000,
-			password: "www.freelavalink.ga",
+			host: "lavalink.oops.wtf", //- The host name or IP of the lavalink server.
+			port: 2000, // The port that lavalink is listening to. This must be a number!
+			password: "www.freelavalink.ga",  //- The password of the lavalink server.
 			retryAmount: 10, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
@@ -49,12 +51,12 @@ module.exports = {
 	],
 	embedColor: "#9c59b6", //Color of the embeds, hex supported
 	presence: {
-		//PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
-		status: "online", // You can have online, idle, and dnd(invisible too but it make people think the bot is offline)
+		// PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
+		status: "online", //- You can have online, idle, dnd and invisible (Note: invisible makes people think the bot is offline)
 		activities: [
 			{
-				name: "/play", //Status Text
-				type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
+				name: "/play", //- Status Text
+				type: "LISTENING", //- PLAYING, WATCHING, LISTENING, STREAMING
 			},
 		],
 	},

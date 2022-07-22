0adc8a969ebb4d6a41f923209be841a89f5d4394
cobianic <vitrage@protonmail.com>
2022-07-23 00:58:19 +0300

    1 node instead of 3, updated dependencies

--- config.js
+++ config.js
@@ -23,29 +23,11 @@ module.exports = {
 	// Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
 	nodes: [
 		{
-			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "lavalink.oops.wtf", //- The host name or IP of the lavalink server.
-			port: 2000, // The port that lavalink is listening to. This must be a number!
-			password: "www.freelavalink.ga",  //- The password of the lavalink server.
-			retryAmount: 10, //- The amount of times to retry connecting to the node if connection got dropped.
-			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
-			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
-		},
-		{
-			identifier: "Backup Node #1", //- Used for identifier in stats commands.
+			identifier: "Main node", //- Used for identifier in stats commands.
 			host: "audio.alexanderof.xyz",
 			port: 2000,
 			password: "lavalink",
-			retryAmount: 10, //- The amount of times to retry connecting to the node if connection got dropped.
-			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
-			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
-		},
-		{
-			identifier: "Backup Node #2", //- Used for identifier in stats commands.
-			host: "lava-ny-01.thermalhosting.com",
-			port: 4018,
-			password: "thermalhosting.com",
-			retryAmount: 10, //- The amount of times to retry connecting to the node if connection got dropped.
+			retryAmount: 100, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
 		},

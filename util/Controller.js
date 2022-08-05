d8d2e66d496bf0c4ee1ef7b7cc2387094a2c8b93
❥KiyoshiდKunツ <kiyoshikuncool@gmail.com>
2022-08-05 11:20:49 +0700

    Improve Controller & Clean all leftover (#1023)
    * Update controller & Delete all left over
    
    * The trackRepeat icon and queueRepeat icon will be more separate
    * "Queue end" and "Disconnected" will be delete after amount of time
    
    * Update voiceStateUpdate.js
    
    Fix #1013 (The bot will pause when no one is in the voice channel if loop mode is on)
    
    * Update Controller.js
    
    * If there is no song in the queue, when you press "Next", the bot will automatically exit and nofi. And if there are songs left, the bot will jump to the next song!
    * Update queueTrack console log
    * Replace nofi that there is no previous to disable the "Replay" button
    
    * Update handler for Controller
    
    * Disable "Replay" button when there is no previous song in queue, instead of nofi them
    
    * Update voiceStateUpdate.js
    
    Fix SudhanPlayz#1013 (The bot will pause when no one is in the voice channel if loop mode is on)
    # Conflicts:
    #	lib/DiscordMusicBot.js
    #	util/Controller.js

--- util/Controller.js
+++ util/Controller.js
@@ -6,11 +6,11 @@ const load = require("lodash");
  * @param {import("../lib/DiscordMusicBot")} client
  * @param {import("discord.js").ButtonInteraction} interaction
  */
-module.exports = async (client, interaction, options) => {
+module.exports = async (client, interaction) => {
 	let guild = client.guilds.cache.get(interaction.customId.split(":")[1]);
 	let property = interaction.customId.split(":")[2];
 	let player = client.manager.get(guild.id);
-	
+
 	if (!player) {
 		await interaction.reply({
 			embeds: [
@@ -30,7 +30,7 @@ module.exports = async (client, interaction, options) => {
 			);
 		return interaction.reply({ embeds: [joinEmbed], ephemeral: true });
 	}
-	
+
 	if (
 		interaction.guild.me.voice.channel &&
 		!interaction.guild.me.voice.channel.equals(interaction.member.voice.channel)
@@ -40,12 +40,13 @@ module.exports = async (client, interaction, options) => {
 			.setDescription(
 				"❌ | Хазяїн, ви не в моєму голосовому каналі",
 			);
-		return interaction.reply({ embeds: [sameEmbed], ephemeral: true });
+		return await interaction.reply({ embeds: [sameEmbed], ephemeral: true });
 	}
-	
+
 	if (property === "Stop") {
 		player.queue.clear();
 		player.stop();
+		player.set("autoQueue", false);
 		client.warn(`Player: ${ player.options.guild } | Successfully stopped the player`);
 		const msg = await interaction.channel.send({
 			embeds: [
@@ -55,42 +56,26 @@ module.exports = async (client, interaction, options) => {
 		setTimeout(() => {
 			msg.delete();
 		}, 5000);
-		
+
 		interaction.update({
 			components: [client.createController(player.options.guild, player)],
 		});
-		
 		return;
 	}
-	
+
 	// if theres no previous song, return an error.
 	if (property === "Replay") {
 		const previousSong = player.queue.previous;
 		const currentSong = player.queue.current;
-		const nextSong = player.queue[0];
+		const nextSong = player.queue[0]
 		
-		if (!previousSong
-			|| previousSong === currentSong
-			|| previousSong === nextSong) {
-			const msg = await interaction.channel.send({
-				embeds: [
-					new MessageEmbed()
-						.setColor("RED")
-						.setDescription("Немає попередньої пісні"),
-				],
-			});
-			setTimeout(() => {
-				msg.delete();
-			}, 5000);
-			return interaction.deferUpdate();
-		}
 		if (previousSong !== currentSong && previousSong !== nextSong) {
-			player.queue.splice(0, 0, currentSong);
+			player.queue.splice(0, 0, currentSong)
 			player.play(previousSong);
 			return interaction.deferUpdate();
 		}
 	}
-	
+
 	if (property === "PlayAndPause") {
 		if (!player || (!player.playing && player.queue.totalSize === 0)) {
 			const msg = await interaction.channel.send({
@@ -105,22 +90,41 @@ module.exports = async (client, interaction, options) => {
 			}, 5000);
 			return interaction.deferUpdate();
 		} else {
-			
+
 			if (player.paused) {
 				player.pause(false);
 			} else {
 				player.pause(true);
 			}
 			client.warn(`Player: ${ player.options.guild } | Successfully ${ player.paused? "paused" : "resumed" } the player`);
-			
+
 			return interaction.update({
 				components: [client.createController(player.options.guild, player)],
 			});
 		}
 	}
-	
+
 	if (property === "Next") {
-		player.stop();
+		if((player.queue.totalSize === 0 || !player.queue.totalSize) ||
+		   	(player.trackRepeat || player.queueRepeat)) {
+				const msg = await interaction.channel.send({
+					embeds: [
+					  new MessageEmbed()
+					    .setColor(client.config.embedColor)
+					    .setAuthor({
+					      name: "Черга закінчилась!!",
+					      iconURL: client.config.iconURL,
+					    })
+					    .setFooter({ text: "Черга закінчилась на " })
+					    .setTimestamp(),
+					],
+				      });
+				      setTimeout(() => {
+					msg.delete();
+				      }, 6000);
+		      		player.destroy();
+		      		player.setNowplayingMessage(client, null);
+			} else player.stop();
 		return interaction.deferUpdate();
 	}
 	

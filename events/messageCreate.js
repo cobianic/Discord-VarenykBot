5c8fee3cfa352a14116a451132d1d6ebd25b0e83
cobianic <vitrage@protonmail.com>
2022-08-26 17:09:40 +0300

    Improve messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,18 +1,28 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 
 module.exports = async (client, message) => {
 	const mention = new RegExp(`^<@!?${ client.user.id }>( |)$`);
+	const invite = `https://discord.com/oauth2/authorize?client_id=${ client.config.clientId }&permissions=${ client.config.permissions }&scope=${ client.config.scopes.toString()
+		.replace(/,/g, '%20') }`;
+	
+	const buttons = new MessageActionRow()
+		.addComponents(
+			new MessageButton()
+				.setStyle("LINK")
+				.setLabel('Запросіть мене')
+				.setURL(invite),
+		);
 	
 	if (message.content.match(mention)) {
 		const mentionEmbed = new MessageEmbed()
 			.setColor(client.config.embedColor)
 			.setDescription(
-				`Мій префікс на цьому сервері\`/\` (слеш команда).\nНапишіть \`/help\` щоб побачити всі мої команди.\nЯкщо ви не можете їх побачити, будьласка виженіть і [запросіть](https://discord.com/oauth2/authorize?client_id=${ client.config.clientId }&permissions=${ client.config.permissions }&scope=${ client.config.scopes.toString()
-					.replace(/,/g, "%20") }) мене на сервер ще раз з правильними дозволами.`,
+				`Мій префікс на цьому сервері \`/\` (слеш команда).\nНапишіть \`/help\` щоб побачити всі мої команди.\nЯкщо ви не можете їх побачити, будьласка виженіть і [запросіть](invite) мене на сервер ще раз з правильними дозволами.`,
 			);
 		
 		message.channel.send({
 			embeds: [mentionEmbed],
+			components: [buttons],
 		});
 	}
 };
